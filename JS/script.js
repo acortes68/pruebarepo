160c10ac69802491f37cc19fdf6b65203fe0e20e
acortes68 <arodri68@ibero.edu.co>
2020-10-17 23:59:44 -0500

    Este es el proyecto final de la unidad 2

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+function Confirmacion() {
+    return confirm("¿Quieres ir a la red social?")
+}
+
+function Confirma() {
+    return confirm("¿Quiere ir a esta red social?")
+}
+
+function Afirma() {
+    return confirm("¿Desea ir a esta red social?")
+}
+
+
+
+
+  
